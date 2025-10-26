616bac1fd5912bdf224697b0bfacbdc01266f80a
Deathemonic <zinth.chan@tuta.io>
2025-10-26 17:25:59 +0800

    fix: benchmarks

--- memorypack-benchmarks/rust-benchmarks/benches/memorypack_bench.rs
+++ memorypack-benchmarks/rust-benchmarks/benches/memorypack_bench.rs
@@ -11,7 +11,7 @@ static DEALLOCATED: AtomicUsize = AtomicUsize::new(0);
 
 unsafe impl GlobalAlloc for CountingAlloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let ret = System.alloc(layout);
+        let ret = unsafe { System.alloc(layout) };
         if !ret.is_null() {
             ALLOCATED.fetch_add(layout.size(), Ordering::SeqCst);
         }
@@ -19,7 +19,7 @@ unsafe impl GlobalAlloc for CountingAlloc {
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        System.dealloc(ptr, layout);
+        unsafe { System.dealloc(ptr, layout) };
         DEALLOCATED.fetch_add(layout.size(), Ordering::SeqCst);
     }
 }
